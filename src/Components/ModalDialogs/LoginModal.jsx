9f07d99090d9257946b00bf14796966cf825acb3
Michael Wion <rectangleequals@gmail.com>
2023-05-01 12:58:24 -0700

    Added basic login modal dialog

--- src/Components/ModalDialogs/LoginModal.jsx
+++ src/Components/ModalDialogs/LoginModal.jsx
@@ -1,71 +1,152 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 import resolveUrl from "utils/resolveUrl";
 import getExternalIP from "utils/getExternalIP";
+import { Container, Alert } from "react-bootstrap";
 
 const apiUrlBase = import.meta.env.VITE_API_BASEPATH || "http://localhost/api";
 const apiUrlAuth = resolveUrl(apiUrlBase, 'auth');
 
-const LoginModal = ({ show, onHide }) =>
+const LoginModal = ({ shown, setShowModal, setLoginStatus }) =>
 {
+  const [provider, setProvider] = useState('');
+  const [loginController, setLoginController] = useState(new AbortController());
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [alertMessage, setAlertMessage] = useState({variant: 'info', title: 'Please choose a login provider:', message: ''});
 
-  const handleLogin = async (provider) => {
-    setIsLoading(true);
+  useEffect(() =>
+  {
+    (async() => {
+      // Ignore scenarios where we aren't currently attempting to log in
+      if(!isLoggingIn || provider.length < 1)
+        return;
 
-    const externalIP = await getExternalIP();
-    if(externalIP === '') {
-      console.error('Failed to get external IP!');
-      return;
+      // Get the current IP of the client
+      const externalIP = await getExternalIP();
+      if(externalIP === '') {
+        console.error('Failed to get external IP!');
+        return;
+      }
+
+      // Make an API call to the backend server to initiate the social login flow
+      const queryParams = {
+        provider: provider,
+        from: externalIP
+      };
+      const queryString = Object.keys(queryParams)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
+        .join('&');
+      const authUrl = `${apiUrlAuth}?${queryString}`;
+
+      await fetch(authUrl, {signal: loginController.signal})
+        .then(response => handleResponse(response))
+        .catch(err => {
+          setIsLoggingIn(false);
+          setAlertMessage({variant: 'danger', title: 'Error', message: err.message})
+          setTimeout(() => setShown(false), 3000);
+        });
+    })();
+  }, [isLoggingIn]);
+
+  const abortRequest = () => {
+    loginController.abort('User-initiated login cancellation');
+    setLoginController(new AbortController());
+  }
+
+  const handleClose = () => {
+    if (isLoggingIn)
+      abortRequest();
+    setShown(false);
+  }
+
+  const handleResponse = async(response) => {
+    const data = await response.json();
+
+    // Simulate a delayed request
+    /*
+    if(provider === 'discord') {
+      console.log("Waiting for 4 seconds...");
+      await new Promise(resolve => setTimeout(resolve, 4000));
+      console.log("Done waiting!");
     }
+    */
 
-    // Make an API call to your backend server to initiate the social login flow
-    const queryParams = {
-      provider: provider,
-      from: externalIP
-    };
-    const queryString = Object.keys(queryParams)
-      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
-      .join('&');
-    const authUrl = `${apiUrlAuth}?${queryString}`;
-    const response = await fetch(authUrl);
-    const { url } = await response.json();
+    // Validate that we are still attempting to log in, and that all response data is valid
+    if(isLoggingIn && data && data.status) {
+      // Update login status, then close the dialog
+      const loginStatus = data.status === 'ok';
+      setLoginStatus(loginStatus);
+      setIsLoggingIn(false);
+      if(loginStatus)
+        handleClose();
+      else {
+        setAlertMessage({variant: 'warning', title: 'Login Failed', message: `Server response: ${data.status}`})
+        setTimeout(() => setShown(true), 3000);
+      }
+    }
+  }
 
-    // Redirect the user to the social login page
-    window.location.href = url;
+  const handleLogin = async (requestedProvider) =>
+  {
+    // Make sure we set the loading state and log out the current user before making a login request
+    setIsLoading(true);
+    setProvider(requestedProvider);
+    setLoginStatus(false);
+    setIsLoggingIn(true);
   };
 
+  const setShown = (shown) => {
+    setAlertMessage({variant: 'info', title: 'Please choose a login provider:', message: ''});
+    setShowModal(shown);
+    setIsLoading(false);
+  }
+
   return (
-    <Modal show={show} onHide={onHide}>
-      <Modal.Header closeButton>
+    <Modal show={shown} onHide={handleClose} centered>
+      <Modal.Header>
         <Modal.Title>Login</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Button
-          variant="primary"
-          block
-          disabled={isLoading}
-          onClick={() => handleLogin("discord")}
-        >
-          {isLoading ? "Loading..." : "Login with Discord"}
-        </Button>
-        <Button
-          variant="primary"
-          block
-          disabled={isLoading}
-          onClick={() => handleLogin("google")}
-        >
-          {isLoading ? "Loading..." : "Login with Google"}
-        </Button>
-        <Button
-          variant="primary"
-          block
-          disabled={isLoading}
-          onClick={() => handleLogin("steam")}
-        >
-          {isLoading ? "Loading..." : "Login with Steam"}
-        </Button>
+        <Container>
+          <Alert variant={alertMessage.variant}>
+            <Alert.Heading>{alertMessage.title}</Alert.Heading>
+            {alertMessage.message}
+          </Alert>
+        </Container>
       </Modal.Body>
+      <Modal.Footer>
+        <Container className="d-flex flex-column align-items-center">
+          <Button
+            className="flex-grow-1 w-100 mb-1"
+            variant="primary"
+            disabled={isLoading}
+            onClick={() => handleLogin("discord")}>
+            {isLoading ? "Loading..." : "Discord"}
+          </Button>
+          <Button
+            className="flex-grow-1 w-100 mb-1"
+            variant="primary"
+            disabled={isLoading}
+            onClick={() => handleLogin("steam")}> 
+            {isLoading ? "Loading..." : "Steam"}
+          </Button>
+          <Button
+            className="flex-grow-1 w-100 mb-1"
+            variant="primary"
+            disabled={isLoading}
+            onClick={() => handleLogin("microsoft")}> 
+            {isLoading ? "Loading..." : "Microsoft"}
+          </Button>
+          <Button
+            className="flex-grow-1 w-100"
+            variant="primary"
+            disabled={isLoading}
+            onClick={() => handleLogin("epic")}> 
+            {isLoading ? "Loading..." : "Epic Games"}
+          </Button>
+        </Container>
+      </Modal.Footer>
     </Modal>
   );
 };
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -7,16 +7,28 @@ import './Home.css';
 function Home()
 {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [wasLoggedIn, setWasLoggedIn] = useState(isLoggedIn);
   const [showModal, setShowModal] = useState(false);
 
-  const handleLogin = () => {
+  const handleLoginRequest = () => {
     setShowModal(true);
   };
   
-  const handleLogout = () => {
-    setIsLoggedIn(false);
+  const handleLogoutRequest = () => {
+    console.log('handleLogout');
+    if(isLoggedIn)
+      handleLoginChange(false);
+    setShowModal(false);
   };
-  
+
+  const handleLoginChange = (loginStatus) => {
+    setIsLoggedIn(loginStatus);
+    if(loginStatus != wasLoggedIn) {
+      console.log('handleLoginChange: ' + loginStatus);
+    }
+    setWasLoggedIn(loginStatus);
+  }
+ 
   return (
     <div className="d-flex flex-column min-vh-100 min-vw-100 border-bottom border-primary">
       
@@ -36,12 +48,12 @@ function Home()
             <Navbar.Collapse id="navbar-nav" className="justify-content-end align-items-center">
               <Nav>
                 {isLoggedIn ? (
-                  <Nav.Link href="#" className='d-flex flex-row justify-content-center align-items-center' onClick={handleLogout}>
+                  <Nav.Link href="#" className='d-flex flex-row justify-content-center align-items-center' onClick={handleLogoutRequest}>
                     <span className="user-name">Logout</span>
                     <ImageAsset className="asset-user-avatar img-user-avatar img-show-border" />
                   </Nav.Link>
                 ) : (
-                  <Nav.Link href="#" className='d-flex flex-row justify-content-center align-items-center' onClick={handleLogin}>
+                  <Nav.Link href="#" className='d-flex flex-row justify-content-center align-items-center' onClick={handleLoginRequest}>
                     <span className="discord-name">Login</span>
                     <ImageAsset className="asset-discord-logo img-discord-logo img-show-border" />
                   </Nav.Link>
@@ -51,7 +63,7 @@ function Home()
 
           </Container>
         </Navbar>
-        <LoginModal show={showModal} onHide={() => setShowModal(false)} />
+        <LoginModal shown={showModal} setShowModal={setShowModal} setLoginStatus={handleLoginChange} />
       </header>
 
       {/* Main Content */}
