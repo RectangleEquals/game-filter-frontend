ebc76a40bb3c42d3ec0349e04c18dfee1025b744
Michael Wion <rectangleequals@gmail.com>
2023-05-28 08:29:05 -0700

    Nearly fixed Navbar height offset issues

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -1,25 +1,23 @@
-import useAuthContext from 'components/AuthContext/AuthContext';
 import useNavbarContext from 'components/NavbarContext/NavbarContext';
-import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 
-const MainContent = ({ children, initialMarginTop }) => {
-  const authContext = useAuthContext();
+const MainContent = ({ children }) => {
   const navbarContext = useNavbarContext();
-  const [marginTop, setMarginTop] = useState(initialMarginTop || 65.5);
-
-  useEffect(_ => {
-    setMarginTop(navbarContext.navbarHeight);
-  }, [navbarContext.navbarHeight]);
-
-  useEffect(_ => {
-    authContext.logWarning(`[MainContent] > marginTop: ${marginTop}`);
-  }, [marginTop]);
 
   return (
-    <Container fluid className="main-content flex-grow-1 d-flex flex-column justify-content-center align-items-center" style={{marginTop: marginTop}}>
-      {children}
-    </Container>
+    <>
+      {/* Dummy header which sits behind navbar, copying it's height */}
+      <Container fluid className="main-content-head" style={{
+        height: navbarContext.navbarHeight,
+        transition: "height 0.2s ease-in-out"
+      }}/>
+      {/* Main content which sits directly below the dummy header, giving
+        the illusion that it's "connected" to the bottom of the navbar */}
+      <Container fluid
+        className="main-content flex-grow-1 d-flex flex-column justify-content-center align-items-center">
+        {children}
+      </Container>
+    </>
   );
 };
 
